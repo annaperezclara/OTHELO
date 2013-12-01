1d5757eb407aa1f239d7de1252a7d773d76bc725
Julien Lambert <lambertj123@gmail.com>
2013-12-01 19:56:06 +0100

    divers

---
+++ Othelo/const.h
@@ -0,0 +1,14 @@
+//
+//  const.h
+//  Othelo
+//
+//  Created by Julien Lambert on 01/12/2013.
+//  Copyright (c) 2013 Julien Lambert. All rights reserved.
+//
+
+#ifndef Othelo_const_h
+#define Othelo_const_h
+
+
+
+#endif
